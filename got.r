bcb5075f4a7af16002ffdcfb31da92876b94ab0e
Chia Liu <ccl26@st-andrews.ac.uk>
2023-02-07 15:08:09 +0000

    Update got.r

--- got.r
+++ got.r
@@ -1,44 +1,49 @@
-## This code is written by Chia Liu on February 7th, 2023 ## 
-
+## This code is written by Chia Liu on February 7th, 2023 for the Population & Health Research Group at the University of St. Andrews ## 
 library(tidyverse)
 library(survival)
 library(survminer)
 
-## remember to run install.packages('whateverpackage') for whatever package you don't already have installed
-## before you call the library function
-
-data<-read.csv('C:/Users/ccl26/OneDrive - University of St Andrews/R_coef/clean_got.csv')
+## Read in the data 
+## Thank you Dr. Lystad for sharing your blood sweat and tears from binge watching seven seasons of Game of Thrones
+data<-read.csv('path/clean_got.csv')
 
 ## How do we work with text data? A story for another time ##
-# data %>% mutate(x=ifelse(grepl('^decapitat',dth_description,ignore.case=T)==T,1,0)) %>% filter(x==1)
+## But if you really want to know who got decapitated on GoT...
+data %>% mutate(x=ifelse(grepl('^decapitat',dth_description,ignore.case=T)==T,1,0)) %>% filter(x==1)
 
-## create survival object 
+## Steps: 1. create survival object 2. run regressions 3. extract coefficients and lower/upper estimates 4. plot
+
+## Create survival object 
 eha<-data %>% 
   mutate(event=dth_flag,
          dur=pmin(dth_time_hrs,censor_time_hrs),
          Survobj=Surv(dur,event))
 fit<-survfit(Survobj~sex, data=eha)
 
+## KM curves tell us that men are dying at a higher rate in GoT 
 ggsurvplot(fit,conf.int=T,xlab='Running time in hours',xlim=c(0,60),break.time.by=5)
 
+## Run Cox proportional hazards models
 c1<-coxph(Surv(dur,event)~sex,data=eha)
 c2<-coxph(Surv(dur,event)~sex+social_status,data=eha)
 c3<-coxph(Surv(dur,event)~sex+social_status+allegiance_switched,data=eha)
 c4<-coxph(Surv(dur,event)~sex+social_status+allegiance_switched+allegiance_last,data=eha)
 summary(c4)
 
-## There are many ways to do this
-
-## straight out of the box option: ggforest 
-## only works for coxph class
-## gives you the result in Hazard ratio, and already on log scale! 
+## There are many ways to plot these
+## Straight out of the box option--> ggforest 
+## This is what the authors used in the GoT paper 
+## ggforest gives you the result in hazard ratio, and already on a log scale! 
 ## but not a lot of add on options. 
+## ONLY WORKS FOR CLASS COXPH!
 ## You can try to tweak the function using trace(survminer::ggforest, edit = T) 
 ggforest(c4,
          main='CoxPH hazard ratio of GoT deaths',
          fontsize=0.8,data=eha)
 
-## but maybe we want to show coefficients of multiple models together
+## But maybe we want to show coefficients of multiple models together
+## There are many options in using packages to plot coefficients 
+## These are just some quick stabs, check out their documentations and play around with them 
 library(dotwhisker)
 dwplot(list(c1,c2,c3),
        vline = geom_vline(
@@ -51,20 +56,15 @@ dwplot(list(c1,c2,c3),
            allegiance_switchedyes="Switched allegiance"))+
   theme_bw(base_size=12)
 
-## let's try another one 
+## Let's try another one 
 library(sjPlot)
 plot_models(c1,c2,c3,m.labels=c('Model 1','Model 2','Model 3'),vline.color = 'red')+
   theme_pubr()
 
-## displayed in another way (less effective, in this case)
-plot_models(c1,c2,c3,m.labels=c('Sex only','Sex & Status','Full model'),grid=T,
-            vline.color='red',show.legend=F) +
-  theme_bw()
+## Ok. Now let's do this from scratch, using ggplot 
+## The main ingredients we need: coefficient, lower & upper limits
 
-## let's plot from scratch, using ggplot 
-## The main ingredients we need: coefficient, lower & upper limit
-
-## first, write a function to extract the information we need from the model objects
+## First, write a function to extract the information we need from the model objects
 getm_f<-function(fit){
   a<-exp(fit$coefficients) %>% round(2)
   b<-exp(confint(fit)) %>% round(2)
@@ -75,17 +75,17 @@ getm_f<-function(fit){
   return(c)
 }
 
-## apply the function on fit c1 to c4 and add a column called model 
+## Apply the function on fits c1 to c4 and add a column called model 
 x1<-getm_f(c1) %>% mutate(model=1)
 x2<-getm_f(c2) %>% mutate(model=2)
 x3<-getm_f(c3) %>% mutate(model=3)
 x4<-getm_f(c4) %>% mutate(model=4)
 
-## stack them together so we're only dealing with one coefficient data frame
+## Stack them together so we're only dealing with one coefficient data frame
 mydata<-rbind(x1,x2,x3,x4)
 
-## make the labels a bit more readable
-## this syntax specifically works with strings 
+## Make the labels a bit more readable
+## This syntax specifically works with strings 
 ## but one can always use a series of if-else statements
 data2<-mydata %>% 
   mutate(names=case_when(
@@ -95,7 +95,7 @@ data2<-mydata %>%
     grepl('^allegiance_la',names)==T~substr(names,16,nchar(names)) 
   ))
 
-## create 4 rows of reference categories 
+## Create 4 rows of reference categories 
 names<-c('Female','Highborn','Did not switch side','Bolton')
 coef<-1
 lower<-1
@@ -103,9 +103,9 @@ upper<-1
 model<-'ref'
 ref<-data.frame(names,coef,lower,upper,model)
 
-## stack the ref categories with our coefficient data frame 
-## use factor to arrange the categories in the order we want them to appear in the plot
-## otherwise it's alphabetic by default
+## Stack the ref categories with our coefficient data frame 
+## Use factor to arrange the categories in the order we want them to appear in the plot
+## otherwise they're arranged alphabetically by default
 data3<-data2 %>% 
   rbind(ref) %>% 
   mutate(model=ifelse(model=='ref','Reference',paste('Model',model)),
@@ -115,7 +115,7 @@ data3<-data2 %>%
                                      'Lowborn','Highborn','Male','Female',
                                      'unknown','other')))
 
-## making the plot 
+## Making the plot 
 fig1<-data3 %>% 
   filter(!names %in% c('unknown','other')) %>%
   ggplot(aes(x=names,y=coef,color=model))+
@@ -132,13 +132,14 @@ fig1<-data3 %>%
         axis.text.x=element_text(size=7),
         axis.text.y = element_text(face = rev(c('bold','plain','bold','plain','bold',
                                                 'plain','bold','plain','plain',
-                                                'plain','plain','plain','plain'))),
+                                                'plain','plain','plain','plain'))), ## this helps us manually bold the references categories
         text=element_text(family='arial',size=12),
         legend.background=element_blank(),
         legend.key = element_rect(fill = "white"),
         legend.title=element_blank())
 
 ## if you want to save the plot, save it as an object (for example 'fig1') then ggsave fig1
+## otherwise ggsave saves the last object that you run
 
 ggsave(plot = fig1, width = 10, height = 4, dpi = 300, filename = "C:/Users/ccl26/OneDrive - University of St Andrews/R_coef/got_plot.png")
 
